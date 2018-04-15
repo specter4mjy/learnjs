35ca57a1c1c26a0592780c141065fc1b967bf7d5
Jony Ma <majunyang@meican.com>
2018-04-15 15:25:51 +0800

    Add skip button

--- public/app.js
+++ public/app.js
@@ -43,6 +43,15 @@ learnjs.problemView = function(data) {
         return false;
     }
 
+    if (problemNumber < learnjs.problems.length) {
+        var buttonItem = learnjs.template('skip-btn');
+        buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
+        $('.nav-list').append(buttonItem);
+        view.bind('removingView', function() {
+            buttonItem.remove();
+        });
+    }
+
     view.find('.check-btn').click(checkAnswerClick);
     view.find('.title').text('Problem #' + problemNumber);
     learnjs.applyObject(problemData, view);
@@ -72,6 +81,7 @@ learnjs.showView = function(hash) {
     var viewFn = routes[hashParts[0]];
 
     if (viewFn) {
+        learnjs.triggerEvent('removingView', []);
         $('.view-container').empty().append(viewFn(hashParts[1]));
     }
 }
@@ -94,4 +104,8 @@ learnjs.appOnReady = function() {
         learnjs.showView(window.location.hash);
     };
     learnjs.showView(window.location.hash);
+}
+
+learnjs.triggerEvent = function(name, args) {
+    $('.view-container>*').trigger(name, args);
 }
